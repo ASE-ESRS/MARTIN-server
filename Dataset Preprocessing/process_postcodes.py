e2c6fd6d677bb15cfae91c78f1276497a4699c09
Loic Verrall <loic.verrall@gmail.com>
2017-11-14 12:10:50 +0000

    Multithreading added and code tidied.

--- Dataset Preprocessing/process_postcodes.py
+++ Dataset Preprocessing/process_postcodes.py
@@ -15,17 +15,14 @@ print 'Opening OutputDataset.csv\n'
 outputDataset = open('OutputDataset.csv', 'w')
 writer = csv.writer(outputDataset, delimiter=',')
 
-def getNextBatch():
-    # Counters to track progress.
-    batchNumber = 1
-    processedCount = 0
+# Counters to track progress.
+batchCount = 0
+processedCount = 0
 
+def getNextBatch():
     # A dictionary containing a batch of 100 entries to process, indexed by postcode.
     # Limited to 100 as that is the API max batch convertion size.
     batch = {}
-
-    # Used for progress reporting.
-    lastPercentShown = 0
 
     for row in reader:
         # Extract the necessary entry information.
@@ -39,67 +36,17 @@ def getNextBatch():
 
         # Check whether the batch is maximum size yet (100).
         if len(batch) >= 100:
-            yield (dict(batch), batchNumber, processedCount)
-
-            # The following code was used to dispatch async tasks but is no longer used.
-            # This is because the server rejects more than 1 request per second, so it's not neeeded.
-            #     async_thread = threading.Thread(target=longProcess, args=[input], kwargs={})
-            #     async_thread.start()
+            # Note: a copy is made to avoid inteference between threads.
+            yield dict(batch)
 
             # Clear the batch list ready for the next thread.
-            processedCount += 100
             batch = {}
 
-            # Progress reporting
-            if processedCount >= 350000:
-                if 100 > lastPercentShown:
-                    print '100%', '-', processedCount, "entries,", batchNumber, "batches\n"
-                    lastPercentShown = 100
-            elif processedCount >= 315000:
-                if 90 > lastPercentShown:
-                    print '90%', '-', processedCount, "entries,", batchNumber, "batches\n"
-                    lastPercentShown = 90
-            elif processedCount >= 280000:
-                if 80 > lastPercentShown:
-                    print '80%', '-', processedCount, "entries,", batchNumber, "batches\n"
-                    lastPercentShown = 80
-            elif processedCount >= 245000:
-                if 70 > lastPercentShown:
-                    print '70%', '-', processedCount, "entries,", batchNumber, "batches\n"
-                    lastPercentShown = 70
-            elif processedCount >= 210000:
-                if 60 > lastPercentShown:
-                    print '60%', '-', processedCount, "entries,", batchNumber, "batches\n"
-                    lastPercentShown = 60
-            elif processedCount >= 175000:
-                if 50 > lastPercentShown:
-                    print '50%', '-', processedCount, "entries,", batchNumber, "batches\n"
-                    lastPercentShown = 50
-            elif processedCount >= 140000:
-                if 40 > lastPercentShown:
-                    print '40%', '-', processedCount, "entries,", batchNumber, "batches\n"
-                    lastPercentShown = 40
-            elif processedCount >= 105000:
-                if 30 > lastPercentShown:
-                    print '30%', '-', processedCount, "entries,", batchNumber, "batches\n"
-                    lastPercentShown = 30
-            elif processedCount >= 70000:
-                if 20 > lastPercentShown:
-                    print '20%', '-', processedCount, "entries,", batchNumber, "batches\n"
-                    lastPercentShown = 20
-            elif processedCount >= 35000:
-                if 10 > lastPercentShown:
-                    print '10%', '-', processedCount, "entries,", batchNumber, "batches\n"
-                    lastPercentShown = 10
-
-            batchNumber += 1
-
-    # If there're any left over entries to send.
+    # If there're any left over entries to send, send them.
     if len(batch) > 0:
-        # processBatch(dict(batch), batchNumber, processedCount)
-        yield (dict(batch), batchNumber, processedCount)
+        yield dict(batch)
 
-def processBatch((batchToProcess, batchNumber, processedCount)):
+def processBatch(batchToProcess):
     # Make an API request to convert these postcodes (this is a blocking call).
     request = requests.post("http://api.postcodes.io/postcodes/", json = {"postcodes" : batchToProcess.keys()})
     response = request.json()
@@ -112,9 +59,8 @@ def processBatch((batchToProcess, batchNumber, processedCount)):
     rowsToWrite = []
 
     for result in response["result"]:
-
         if result["query"] == "" or result["result"] is None:
-            # Ignoring empty result'
+            # Ignoring empty result.
             continue
 
         longitude = result["result"]["longitude"]
@@ -130,9 +76,11 @@ def processBatch((batchToProcess, batchNumber, processedCount)):
 # Writes the supplied rows to the output CSV file.
 # Increments and reports the progress counters.
 def writeRows(rowsToWrite):
+    # Require the lock to write to the CSV.
     with csvWriteLock:
         writer.writerows(rowsToWrite)
 
+    # Increment the progress reporting variables.
     global processedCount
     processedCount += len(rowsToWrite)
 
@@ -146,9 +94,11 @@ previousReport = 0
 
 # Reports (prints) the current progress to the user every 10%.
 def reportProgressIfRequired():
+    global previousReport
     currentProgress = int(100 * round(processedCount / numberOfEntries))
     if currentProgress > previousReport:
         print "CURRENT PROGRESS:", currentProgress
+        previousReport = currentProgress
 
 startTime = time.time()
 
