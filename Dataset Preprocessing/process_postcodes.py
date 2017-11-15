892c9d7f566eaaa8b3f547f674db1808567520bd
Loic Verrall <loic.verrall@gmail.com>
2017-11-15 09:57:45 +0000

    Postcode processor tidied up, bug fixes, documentation added

--- Dataset Preprocessing/process_postcodes.py
+++ Dataset Preprocessing/process_postcodes.py
@@ -3,6 +3,7 @@ import requests, json
 import os, time, threading
 from multiprocessing.pool import ThreadPool as Pool
 
+# This prevents multiple threads from intefering with oneanother while writing.
 csvWriteLock = threading.Lock()
 
 print 'Data/Opening InputDataset.csv'
@@ -19,6 +20,7 @@ writer = csv.writer(outputDataset, delimiter=',')
 batchCount = 0
 processedCount = 0
 
+# A generator to fetch the next batch (of 100 entries) to be procesesed.
 def getNextBatch():
     # A dictionary containing a batch of 100 entries to process, indexed by postcode.
     # Limited to 100 as that is the API max batch convertion size.
@@ -30,7 +32,6 @@ def getNextBatch():
         date = row[2]
         postcode = row[3]
 
-        # Add the entry to the batch.
         # Make a dictionary of postcode to price pairings (used to join price back to entry later and for the API call).
         batch[postcode] = (price, date)
 
@@ -46,6 +47,7 @@ def getNextBatch():
     if len(batch) > 0:
         yield dict(batch)
 
+# Responsible for proceessing the supplied batch. Includes making API call and calling writeRows().
 def processBatch(batchToProcess):
     # Make an API request to convert these postcodes (this is a blocking call).
     request = requests.post("http://api.postcodes.io/postcodes/", json = {"postcodes" : batchToProcess.keys()})
@@ -92,11 +94,11 @@ def writeRows(rowsToWrite):
 # The progress that was last reported (initially 0%).
 previousReport = 0
 
-# Reports (prints) the current progress to the user every 10%.
+# Reports (prints) the current progress to the user every ~10%.
 def reportProgressIfRequired():
     global previousReport
     currentProgress = int(round(100 * processedCount / numberOfEntries))
-    if currentProgress > previousReport + 5:
+    if currentProgress >= previousReport + 5:
         print currentProgress, "%"
         previousReport = currentProgress
 
@@ -104,17 +106,18 @@ startTime = time.time()
 
 print 'Processing postcodes...\n'
 
-threads = Pool(processes = 8)
+# We use 4 threads to attempt to aleviate the bottleneck caused by network I/O.
+threads = Pool(processes = 4)
 
 batchGenerator = getNextBatch()
 
 for batch in batchGenerator:
     threads.map(processBatch, (batch,))
 
-threads.join()
 threads.close()
+threads.join()
 
 stopTime = time.time()
 elapsedTime = stopTime - startTime
 
-print "\n", processedCount, "entries,", batchCount, "batches, processed in ", elapsedTime, "seconds"
+print "\n", processedCount, "entries,", batchCount, "batches, processed in ", round(elapsedTime), "seconds"
